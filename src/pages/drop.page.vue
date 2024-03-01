ab9a302f0fab8dfc7ef1325c414b7c5544758280
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2024-03-01 17:07:53 -0500

    check accelerometer data

--- src/pages/drop.page.vue
+++ src/pages/drop.page.vue
@@ -1,1 +1,20 @@
-<template><div>hi</div></template>
+<script setup lang="ts">
+import { useDeviceMotion } from "@vueuse/core";
+import { ref } from "vue";
+
+const { acceleration, accelerationIncludingGravity } = useDeviceMotion();
+const max = ref(0);
+const maxGrav = ref(0);
+
+// watch(
+//   acceleration,
+//   () => (max.value = Math.max(max.value, acceleration.value?.z ?? 0))
+// );
+</script>
+
+<template>
+  <div>x: {{ acceleration?.x?.toFixed(0) }}</div>
+  <div>y: {{ acceleration?.y?.toFixed(0) }}</div>
+  <div>z: {{ acceleration?.z?.toFixed(0) }}</div>
+  <!-- <div>maxGrav : {{ accelerationIncludingGravity }}</div> -->
+</template>
